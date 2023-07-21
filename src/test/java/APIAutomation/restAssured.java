73fa7c2bfe660751a3aab98475641667566972b8
spande061 <spande061@gmail.com>
2023-07-21 20:37:18 +0530

    commiting mobile automation

--- src/test/java/APIAutomation/restAssured.java
+++ src/test/java/APIAutomation/restAssured.java
@@ -16,32 +16,7 @@ public class restAssured {
     @Test
     void print(){
 
-            //specify base url
-            RestAssured.baseURI=("https://reqres.in/api/users?page=2");
-
-            //Requst object
-            RequestSpecification httprequest = RestAssured.given();
-
-            //Response Object
-            Response response = httprequest.request(Method.GET,"");
-
-            // [print the response
-
-            String GetResponse = response.getBody().asString();
-
-            System.out.println(GetResponse);
-
-           String url="https://reqres.in/api/users?page=2";
-
-          get(url).then().assertThat().body("data.last_name[0]", Is.is("Lawson")).log().all();
-
-
-       // int status_code=  response.getStatusCode();
-
-       // System.out.println(status_code);
-//        JsonPath jasonresponse = resopnse.jsonPath();
-//        System.out.println(jasonresponse.get("city"));
-//        Assert.assertEquals(404,status_code);
+        System.out.println("job1");
         }
     }
 
